0e156079974629cc3b9c7e7b99a104f574cc3e84
Администратор <dg-medvedev@mail.ru>
2019-06-20 13:07:50 +0300

    Dz1

--- app/src/main/java/by/itacademy/pvt/MenuActivity.kt
+++ app/src/main/java/by/itacademy/pvt/MenuActivity.kt
@@ -1,8 +1,10 @@
 package by.itacademy.pvt
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import by.itacademy.pvt.dz1.Dz1Activity
 
 class MenuActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,15 @@ class MenuActivity : Activity() {
             .setOnClickListener {
 
             }
+
+        findViewById<Button>(R.id.dz1)
+            .setOnClickListener {
+                startDz1()
+            }
+    }
+
+    private fun startDz1() {
+        val intent = Intent(this, Dz1Activity::class.java)
+        startActivity(intent)
     }
 }
