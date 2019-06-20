5cdef883f18c3f9761ecfa5f279bbab8457ec1f7
Администратор <dg-medvedev@mail.ru>
2019-06-20 12:49:15 +0300

    Initial commit

---
+++ app/src/main/java/by/itacademy/pvt/MenuActivity.kt
@@ -0,0 +1,17 @@
+package by.itacademy.pvt
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        findViewById<Button>(R.id.dz0)
+            .setOnClickListener {
+
+            }
+    }
+}
