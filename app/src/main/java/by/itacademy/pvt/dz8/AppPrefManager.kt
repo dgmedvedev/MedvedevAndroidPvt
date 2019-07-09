4979274ac3b41fc9f174ba3a31746b0de6b8f8ac
Администратор <dg-medvedev@mail.ru>
2019-07-10 02:24:02 +0300

    Dz8 part1 completed

---
+++ app/src/main/java/by/itacademy/pvt/dz8/AppPrefManager.kt
@@ -0,0 +1,22 @@
+package by.itacademy.pvt.dz8
+
+import android.content.Context
+import android.content.SharedPreferences
+import by.itacademy.pvt.dz8.Dz8StudentListActivity.Companion.SHARED_PREFS_NAME
+import by.itacademy.pvt.dz8.Dz8StudentListActivity.Companion.TEXT_KEY
+
+class AppPrefManager(context: Context) {
+    private val sharedPrefs: SharedPreferences = context
+        .getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun saveUserText(text: String) {
+        sharedPrefs
+            .edit()
+            .putString(TEXT_KEY, text)
+            .apply()
+    }
+
+    fun getUserText(): String {
+        return sharedPrefs.getString(TEXT_KEY, "") ?: ""
+    }
+}
