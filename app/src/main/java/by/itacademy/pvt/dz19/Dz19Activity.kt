83e9efd4669a221543fb043827ce6470070a9e2d
Администратор <dg-medvedev@mail.ru>
2019-08-14 16:54:40 +0300

    dz19

--- app/src/main/java/by/itacademy/pvt/dz19/Dz19Activity.kt
+++ app/src/main/java/by/itacademy/pvt/dz19/Dz19Activity.kt
@@ -10,7 +10,7 @@ class Dz19Activity : Activity() {
     private val subject = PublishSubject.create<String>() // subject - это как LiveData, но более продвинутая
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dz15)
+        setContentView(R.layout.activity_dz19)
     }
 
     override fun onDestroy() {
